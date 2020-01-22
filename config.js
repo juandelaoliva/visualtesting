879e66545c9a46fbb7f22461cd21d42bed484096
Juan de la Oliva Aguilar <juan.oliva@cocomore.com>
2020-01-22 15:39:58 +0100

    delete

--- config.js
+++
@@ -1,7 +0,0 @@
-//change file name to "config.js"
-const token = process.env.token;
-const admin = process.env.admin;
-module.exports = {
-  botToken: token,
-  adminChatId: admin,       //your telegram chat id here, allows usage of /broadcast command
-}
